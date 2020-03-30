c097ee626f09e1e70016a8201dec2d370ff3106c
qloikp <20224416+qloikp@users.noreply.github.com>
2020-03-30 22:20:02 +0200

    fixed router import

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Router } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
